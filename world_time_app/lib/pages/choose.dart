f6d7af0aaf9734b0806ac94e2b2215253e1ed494
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2022-11-30 18:16:21 +0530

    added new project world time app

---
+++ world_time_app/lib/pages/choose.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Loaction extends StatefulWidget {
+  const Loaction({super.key});
+
+  @override
+  State<Loaction> createState() => _LoactionState();
+}
+
+class _LoactionState extends State<Loaction> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Choose Location'),
+        centerTitle: true,
+      ),
+      body: Text('choose loaction screen'),
+    );
+  }
+}
