61c873ccdc4d5dc92aca03fd83e529970ec455bb
OM MAHALE <78585854+ommahale@users.noreply.github.com>
2023-01-31 20:34:59 +0530

    Completed signin view

--- fit_quest/lib/main.dart
+++ fit_quest/lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Signin(),
+    debugShowCheckedModeBanner: false,
+    home: const Signin(),
     theme: ThemeData(
         primaryColor: Colors.blueGrey, backgroundColor: Colors.blueGrey[900]),
   ));
@@ -20,74 +21,97 @@ class _SigninState extends State<Signin> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-          child: SizedBox(
-        width: 360,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.person,
-                    color: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+            child: SizedBox(
+          width: 360,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Image.asset('assets/logo.png'),
+              const TextField(
+                  style: TextStyle(color: Colors.white, fontSize: 16.5),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.person,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      'Enter your email',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                    ),
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              const TextField(
+                  style: TextStyle(color: Colors.white),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      color: Colors.white,
+                    ),
+                    label: Text(
+                      'Enter your password',
+                      style: TextStyle(color: Colors.white, fontSize: 16.5),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(color: Colors.white, width: 2),
+                    ),
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              OutlinedButton(
+                  onPressed: () {},
+                  // ignore: sort_child_properties_last
+                  child: const Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Text(
+                      'Sign in',
+                      style: TextStyle(color: Colors.black, fontSize: 17),
+                    ),
                   ),
-                  label: Text(
-                    'Enter your email',
-                    style: TextStyle(color: Colors.white),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                  )),
+              const SizedBox(
+                height: 5,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Not a member?',
+                    style: TextStyle(color: Colors.white, fontSize: 16.5),
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    borderSide: BorderSide(color: Colors.white, width: 2),
-                  ),
-                )),
-            const SizedBox(
-              height: 20,
-            ),
-            const TextField(
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  prefixIcon: Icon(
-                    Icons.lock,
-                    color: Colors.white,
-                  ),
-                  label: Text(
-                    'Enter your password',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    borderSide: BorderSide(color: Colors.white, width: 2),
-                  ),
-                )),
-            const SizedBox(
-              height: 20,
-            ),
-            OutlinedButton(
-                onPressed: () {},
-                // ignore: sort_child_properties_last
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(color: Colors.black, fontSize: 17),
-                  ),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.white),
-                ))
-          ],
-        ),
-      )),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text('Sign up here',
+                          style: TextStyle(fontSize: 16.5)))
+                ],
+              )
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
